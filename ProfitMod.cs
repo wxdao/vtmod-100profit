b430fb4c431f2de6cfade1acaafaf2b655d8cec0
wxdao <waxiadao@gmail.com>
2020-01-29 19:45:21 +0800

    Initial commit

---
+++ ProfitMod.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using VoxelTycoon.Serialization;
+using VoxelTycoon.Tracks;
+
+public class ProfitMod : VoxelTycoon.Modding.IMod
+{
+    public void OnBeforeGameLoad()
+    {
+        var harmony = new Harmony("wxdao.100profit");
+        harmony.PatchAll();
+    }
+
+    public void OnGameLoaded()
+    {
+    }
+
+    public void Read(StateBinaryReader reader)
+    {
+    }
+
+    public void Write(StateBinaryWriter writer)
+    {
+    }
+}
+
+[HarmonyPatch(typeof(VehicleStation), "OnInvalidateSiblings")]
+class VehicleStation_OnInvalidateSiblings_ProfitPatch
+{
+    static void Postfix(List<VehicleStation.Customer> ____customers)
+    {
+        var oldList = new List<VehicleStation.Customer>(____customers);
+        ____customers.Clear();
+        foreach (var c in oldList)
+        {
+            ____customers.Add(new VehicleStation.Customer
+            {
+                Demand = c.Demand,
+                NormalizedProfit = 1,
+            });
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# 100% Profit Mod for VoxelTycoon
+
+Note: To build the project, Assembly-CSharp.dll and UnityEngine.CoreModule.dll from the game must be manually placed in libs/ directory. I'm not committing them since it may violate the EULA.
